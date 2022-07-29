bb83b40a0484a75cdc399c965f01e13eed4602e8
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-07-29 17:08:11 +0530

    fixed the linters

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
-import renderer from 'react-test-renderer'
-import Header from '../components/Header'
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Header from '../components/Header';
 
 describe('Header ComponentTtest', () => {
   test('Output', () => {
     const header = renderer.create(
-        <MemoryRouter>
-            <Header />
-        </MemoryRouter>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
     ).toJSON();
 
     expect(header).toMatchSnapshot();
-  })    
-})
+  });
+});
--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,20 +1,20 @@
 import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer'
-import store from '../app/store'
-import Home from '../components/Home'
+import renderer from 'react-test-renderer';
+import store from '../app/store';
+import Home from '../components/Home';
 
 const HomeStore = () => (
-    <Provider store={store}>
-        <Home />
-    </Provider>
+  <Provider store={store}>
+    <Home />
+  </Provider>
 );
 
 describe('Test Home Components', () => {
-    it('Output', () => {
-        const home = renderer.create(
-            <HomeStore />
-        ).toJSON();
+  it('Output', () => {
+    const home = renderer.create(
+      <HomeStore />,
+    ).toJSON();
 
-        expect(home).toMatchSnapshot();
-    })
-})
+    expect(home).toMatchSnapshot();
+  });
+});
